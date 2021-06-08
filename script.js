72c4a12ba6458b6a79648645f1d2152decf84754
Rico McCoy <68340447+rmccoy2k@users.noreply.github.com>
2021-06-08 02:39:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,54 @@
 // Remember, we're gonna use .textontentstrict mode in all scripts now!
+
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉Correct Number!';
 
+document.querySelector('.number').textContent = 13;
+document.querySelector('.number').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+const x = function () {
+  console.log(23);
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🤬 No number!';
+  }
+});
+// Remember, we're gonna use .textontentstrict mode in all scripts now!
+
+'use strict';
+
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.number').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+const x = function () {
+  console.log(23);
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🤬 No number!';
+  }
+});
